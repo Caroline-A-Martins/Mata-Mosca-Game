4052351b298b606c90dad7512bd37b448d075ff6
Caroline-A-Martins <business.carolmartins@gmail.com>
2024-09-15 19:17:34 -0300

    add

--- game.js
+++ game.js
@@ -1,101 +1,96 @@
-var altura = 0;
-var largura = 0;
-var vidas = 1; 
-var tempo = 15;
+var altura = 0
+var largura = 0
+var vidas = 1
+var tempo = 15
+var criarMosquitoTempo = 1500
 
-var criarMosquitoTempo = 1500;
-
-var nivel = window.location.search;
-nivel = nivel.replace('?', '');
+var nivel = window.location.search.replace('?', '')
 
 if (nivel === 'normal') {
-    criarMosquitoTempo = 1500;
-    vidas = 3;
+    criarMosquitoTempo = 1500
 } else if (nivel === 'dificil') {
-    criarMosquitoTempo = 1000;
-    vidas = 3;
+    criarMosquitoTempo = 1000
 } else if (nivel === 'hard') {
-    criarMosquitoTempo = 750;
-    vidas = 3;
+    criarMosquitoTempo = 750
 } else if (nivel === 'chucknorris') {
-    criarMosquitoTempo = 200; 
-    vidas = 1;
+    criarMosquitoTempo = 350
 }
 
 function ajustaTamanhoPalcoJogo() {
-    altura = window.innerHeight;
-    largura = window.innerWidth;
-    console.log(largura, altura);
+    altura = window.innerHeight
+    largura = window.innerWidth
+    console.log(largura, altura)
 }
 
-ajustaTamanhoPalcoJogo();
+ajustaTamanhoPalcoJogo()
 
 var cronometro = setInterval(function () {
-    tempo -= 1;
+    tempo -= 1
     if (tempo < 0) {
-        clearInterval(cronometro);
-        clearInterval(criaMosquito);
-        window.location.href = 'vitoria.html';
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'vitoria.html'
     } else {
-        document.getElementById('cronometro').innerHTML = tempo;
+        document.getElementById('cronometro').innerHTML = tempo
     }
-}, 1000);
+}, 1000)
+
+var criaMosquito = setInterval(function () {
+    posicaoRandomica()
+}, criarMosquitoTempo)
 
 function posicaoRandomica() {
     if (document.getElementById('mosquito')) {
-        document.getElementById('mosquito').remove();
-
-        if (vidas > 1) { // Altere a condição para verificar se a vida é maior que 1
-            document.getElementById('v' + vidas).src = "img/coracao_vazio.png";
-            vidas++;
+        document.getElementById('mosquito').remove()
+        if (vidas > 3) {
+            window.location.href = 'game_over.html'
         } else {
-            window.location.href = 'game_over.html'; // Redireciona para a página de game over
+            document.getElementById('v' + vidas).src = 'img/coracao_vazio.png'
+            vidas++
         }
     }
 
-    var posicaoX = Math.floor(Math.random() * largura) - 90;
-    var posicaoY = Math.floor(Math.random() * altura) - 90;
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
-    posicaoX = posicaoX < 0 ? 0 : posicaoX;
-    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-    console.log(posicaoX, posicaoY);
+    console.log(posicaoX, posicaoY)
 
-    var mosquito = document.createElement('img');
-    mosquito.src = 'img/mosquito.png';
-    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatotio();
-    mosquito.style.left = posicaoX + 'px';
-    mosquito.style.top = posicaoY + 'px';
-    mosquito.style.position = 'absolute';
-    mosquito.id = 'mosquito';
+    var mosquito = document.createElement('img')
+    mosquito.src = 'img/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
     mosquito.onclick = function () {
-        this.remove();
+        this.remove()
     }
 
-    document.body.appendChild(mosquito);
+    document.body.appendChild(mosquito)
 }
 
 function tamanhoAleatorio() {
-    var classe = Math.floor(Math.random() * 3);
-
+    var classe = Math.floor(Math.random() * 3)
     switch (classe) {
         case 0:
-            return 'mosquito1';
+            return 'mosquito1'
         case 1:
-            return 'mosquito2';
+            return 'mosquito2'
         case 2:
-            return 'mosquito3';
+            return 'mosquito3'
     }
 }
 
-function ladoAleatotio() {
-    var classe = Math.floor(Math.random() * 2);
-
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
     switch (classe) {
         case 0:
-            return 'ladoA';
+            return 'ladoA'
         case 1:
-            return 'ladoB';
+            return 'ladoB'
     }
 }
